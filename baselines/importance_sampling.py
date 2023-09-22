0811dc0e9e31183a8863ce75eda2aaa74a90e641
yuxuan <yuxuanzh@umich.edu>
2023-09-22 09:46:02 -0500

    add multiprocessing to accelerate

--- baselines/importance_sampling.py
+++ baselines/importance_sampling.py
@@ -4,6 +4,8 @@ from typing import List, Callable
 from tqdm import tqdm
 from scipy import stats
 import os
+from multiprocessing import Pool, Lock
+import sys
 
 # construct the proposal distribution by normalizing the proxy score
 # construct tne likelihood ratio by dividing the desired distribution by proposal distribution
@@ -31,9 +33,15 @@ def weighted_sampling(full_size: int, weights: List[float], size: int):
     sample_indexes = np.random.choice(full_size, size=size, p=weights, replace=True)
     return sample_indexes
 
+def join_job(args):
+    table1, table2, condition_evaluator, proposal, likelihood_ratios, sample_ratio, confidence_level, repeats, seed = args
+    join(table1=table1, table2=table2, condition_evaluator=condition_evaluator, proposal=proposal,
+         likelihood_ratios=likelihood_ratios, sample_ratio=sample_ratio, confidence_level=confidence_level, repeats=repeats,
+         seed=seed)
+
 # join based on importance sampling
 def join(table1: List[int], table2: List[int], condition_evaluator: Callable[[int, int], bool],
-         proposal, likelihood_ratios, 
+         proposal, likelihood_ratios,
          sample_ratio: float, confidence_level: float=0.95, repeats: int=10, seed: int=3):
     # generate a full join
     full_join = [[id1, id2] for id1 in table1 for id2 in table2]
@@ -45,7 +53,7 @@ def join(table1: List[int], table2: List[int], condition_evaluator: Callable[[in
     ci_upper_bounds = []
     sample_means = []
     
-    for _ in tqdm(range(repeats)):
+    for _ in range(repeats):
         # importance sampling
         # flatten join likelihood ratios
         flattened_proposal = []
@@ -74,7 +82,51 @@ def join(table1: List[int], table2: List[int], condition_evaluator: Callable[[in
         ci_upper_bounds.append(ci.high)
         sample_means.append(sample_mean)
 
+        sample_mean = np.average(sample_means)
+        ci_lower = np.average(ci_lower_bounds)
+        ci_upper = np.average(ci_upper_bounds)
+        print(sample_ratio, sample_mean, ci_lower, ci_upper)
+
+        
+        if os.path.exists("importance.csv"):
+            f = open("importance.csv", 'a')
+            writer = csv.writer(f)
+        else:
+            f = open("importance.csv", 'w')
+            writer = csv.writer(f)
+            writer.writerow(["sample_ratio", "mean", "ci_lower", "ci_upper"])
+
+        writer.writerow([sample_ratio, sample_mean, ci_lower, ci_upper])
+        f.close()
+        lock.release()
+
     return np.average(sample_means), np.average(ci_lower_bounds), np.average(ci_upper_bounds)
+
+# define evaluator
+positive_labels = set()
+with open("../datasets/oracle_positive_labels.csv") as f:
+    reader = csv.reader(f)
+    _ = next(reader)
+    for row in reader:
+        qid1, qid2 = row
+        positive_labels.add(f"{qid1}|{qid2}")
+
+def condition_evaluator(qid1, qid2):
+    evaluate_pair = None
+    if qid1 > qid2:
+        evaluate_pair = f"{qid2}|{qid1}"
+    elif qid1 < qid2:
+        evaluate_pair = f"{qid1}|{qid2}"
+    else:
+        return False
+    if evaluate_pair in positive_labels:
+        return True
+    else:
+        return False
+    
+def init_pool_process(_lock):
+    global lock
+    lock = _lock
 
 def run_importance(sample_ratios: List[float], dataset: str="QQP", size: int=3500):
     if dataset == "QQP":
@@ -92,52 +144,19 @@ def run_importance(sample_ratios: List[float], dataset: str="QQP", size: int=350
                     break
 
         proposal, likelihood = construct_proposal_likelihood("../datasets/proxy_cosine_qqp.csv")
-        
-        # define evaluator
-        positive_labels = set()
-        with open("../datasets/oracle_positive_labels.csv") as f:
-            reader = csv.reader(f)
-            _ = next(reader)
-            for row in reader:
-                qid1, qid2 = row
-                positive_labels.add(f"{qid1}|{qid2}")
 
         groundtruth = len(positive_labels) * 2 / (3500*3500)
         print(f"query groundtruth is {groundtruth}, {len(positive_labels)*2}")
-
-        def condition_evaluator(qid1, qid2):
-            evaluate_pair = None
-            if qid1 > qid2:
-                evaluate_pair = f"{qid2}|{qid1}"
-            elif qid1 < qid2:
-                evaluate_pair = f"{qid1}|{qid2}"
-            else:
-                return False
-            if evaluate_pair in positive_labels:
-                return True
-            else:
-                return False
             
-        # run join
-        means, ci_lowers, ci_uppers = [], [], []
-        for sample_ratio in sample_ratios:
-            mean, ci_lower, ci_upper = join(qids, qids, condition_evaluator, sample_ratio=sample_ratio, proposal=proposal, likelihood_ratios=likelihood, repeats=100)
-            means.append(mean)
-            ci_lowers.append(ci_lower)
-            ci_uppers.append(ci_upper)
-            print(sample_ratio, mean, ci_lower, ci_upper)
+        lock = Lock()
 
-        # store results
-        if os.path.exists("importance.csv"):
-            f = open("importance.csv", 'a')
-            writer = csv.writer(f)
-        else:
-            f = open("importance.csv", 'w')
-            writer = csv.writer(f)
-            writer.writerow(["sample_ratio", "mean", "ci_lower", "ci_upper"])
-        for sample_ratio, mean, ci_lower, ci_upper in zip(sample_ratios, means, ci_lowers, ci_uppers):
-            writer.writerow([sample_ratio, mean, ci_lower, ci_upper])
-        f.close()
+        pool = Pool(6, initializer=init_pool_process, initargs=(lock,))
+
+        args = [[qids, qids, condition_evaluator, proposal, likelihood, sample_ratio, 0.95, 10, 233] for sample_ratio in sample_ratios]
+        # run join
+        for i, _ in enumerate(pool.map(join_job, args)):
+            sys.stderr.write('\rpercentage of sample ratios completed: {0:%}'.format(i/len(args)))
+        
 
 if __name__ == "__main__":
     sample_ratios = [0.00001*x for x in range(1,100)] + [0.001*x for x in range(1,100)] + [0.2, 0.5, 0.75, 1]
