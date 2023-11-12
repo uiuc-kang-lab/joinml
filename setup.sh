7a4e9e9fc4d0b04c3b210da2c61bb77e6f674d41
yuxuan18 <yuxuan18@node0.text.joinml-pg0.wisc.cloudlab.us>
2023-11-12 12:18:01 -0600

    add setup for cloudlab

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+source ~/.bashrc
+conda create -n joinml
+conda activate joinml
+sudo apt-get update
+sudo apt-get install -y libgl1
+conda install -y python=3.10.12
+pip install -r requirements.txt
+pip install -e .
