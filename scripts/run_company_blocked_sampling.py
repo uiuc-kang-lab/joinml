e815729de636be50c0a0a3a8ec2b757832e7d9d3
yuxuan18 <yuxuan18@node1.uniform.joinml-pg0.wisc.cloudlab.us>
2023-11-21 15:24:13 -0600

    add scripts for company bs

--- scripts/run_company_blocked_sampling.py
+++ scripts/run_company_blocked_sampling.py
@@ -6,13 +6,15 @@ config = Config(
     dataset_name="company",
     proxy="all-MiniLM-L6-v2",
     is_self_join=False,
-    log_path="logs/company-blocking_sampling.log",
+    log_path="logs/company-blocking_sampling-sqrt.log",
     repeats=20,
     device="cpu",
     cache_path="/mydata/yuxuan",
     proxy_score_cache=True,
     sample_size=[5000000, 2500000, 1000000, 750000, 500000, 250000, 100000],
-    blocking_size=[1000000, 500000, 100000, 50000, 10000]
+    blocking_size=[1000000, 500000, 100000, 50000, 10000],
+    defensive_rate=0,
+    proxy_normalizing_style="sqrt"
 )
 
 run(config)
